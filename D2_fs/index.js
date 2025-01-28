7a4bc3e7bcbe160aa302d9714fbe0a2a1f744f60
Kashyap surani <152683782+Kashyapsurani@users.noreply.github.com>
2025-01-28 15:52:42 +0530

    node

---
+++ D2_fs/index.js
@@ -0,0 +1,40 @@
+const http = require("http");
+const port = 3000;
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  switch (req.url) {
+    case "/":
+      fs.readFile("./index.html", (err, data) => {
+        if (!err) {
+          res.write(data);
+        }
+        res.end();
+      });
+      break;
+    case "/about.html":
+      fs.readFile("./about.html", (err, data) => {
+        if (!err) {
+          res.write(data);
+        }
+        res.end();
+      });
+      break;
+    case "/contact.html":
+      fs.readFile("./contact.html", (err, data) => {
+        if (!err) {
+          res.write(data);
+        }
+        res.end();
+      });
+      break;
+    default:
+      res.write("404 Not Found");
+      res.end();
+      break;
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
