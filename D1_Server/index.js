87209a715e41569edba4c6cb4914e9f35c3f436c
Kashyap surani <152683782+Kashyapsurani@users.noreply.github.com>
2025-03-21 13:50:44 +0530

    node

--- D1_Server/index.js
+++ D1_Server/index.js
@@ -1,15 +1,23 @@
-const http = require('http');
-const port = 3000;
+const http = require("http");
+const port = 8000;
 
 const server = http.createServer((req, res) => {
-    res.write('Hello, World!');
-    res.end();
+
+  if (req.url === "/") {
+    res.end("Welcome to the Home Page!");
+  } else if (req.url === "/about") {
+    res.end("This is the About Page.");
+  } else if (req.url === "/contact") {
+    res.end("This is the contect Page.");
+  } else {
+    res.end("404 Not Found");
+  }
 });
 
 server.listen(port, (err) => {
-    if (err) {
-        console.log(`Error: ${err}`);
-        return false;
-    }
-    console.log('Server running at port' +port);
+  if (err) {
+    console.log(`Error: ${err}`);
+    return false;
+  }
+  console.log(`Custom Server running at http://localhost:${port}`);
 });
